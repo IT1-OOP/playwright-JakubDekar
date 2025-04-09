53bc49c8cc70d55b665e5b041f1728ea9a2a2bde
JakubDekar <94068114+JakubDekar@users.noreply.github.com>
2025-04-09 12:21:57 +0200

    sdfsa

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -6,13 +6,10 @@ def test_page_title():
     cesta = os.path.abspath("index.html")
     cesta = urllib.parse.quote(cesta)
 
-   
-    
-
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(f"file://{cesta}")
-        nadpis_1=page.locator("h1").first()
+        nadpis_1=page.locator("h1").first
         expect(nadpis_1).to_be_visible()
         browser.close()
